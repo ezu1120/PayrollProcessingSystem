d079c6dd95293363827a195b8e318b92ddc72483
Faadil1234 <faadilahmed96@gmail.com>
2025-03-15 01:55:19 +0300

    present , support and payrolls file updated

--- back_end/admin/payrolls.php
+++ back_end/admin/payrolls.php
@@ -1,13 +1,13 @@
 <?php
-	include '../connection.php';
-	// Initialize session
-	session_start();
+    include '../connection.php';
+    // Initialize session
+    session_start();
     $id = $_SESSION['id'];
 
-	if ($_SESSION['loggedin'] !== TRUE) {
-		header('location: ../login.php');
-		exit;
-	}
+    if ($_SESSION['loggedin'] !== true) {
+        header('location: ../login.php');
+        exit;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -22,8 +22,15 @@
 <body>
     <div style="overflow: hidden; height: 100vh;">
         <div class="header">
-            <div class="logo">
-                <img src="../images/pms_logo.jpeg" alt="pms_logo" width="85%">
+
+              <div class="app-header-left" style="padding-right: 10px; padding-left:10px; padding-bottom:10px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="feather feather-list">
+                            <line x1="8" y1="6" x2="21" y2="6" />
+                            <line x1="4" y1="12" x2="32" y2="12" />
+                            <line x1="8" y1="18" x2="21" y2="18" />
+                        </svg>
             </div>
             <p>Payroll Management System</p>
             <a href="../index.html">Home</a>
@@ -35,37 +42,37 @@
             <div class="bg_sidebar">
                 <div class="user">
 
-                    <?php
-                    $img = mysqli_query($con,"select picture from users where user_id = $id "); // fetch data from database
+                <?php
+                    $img = mysqli_query($con, "select picture from users where user_id = $id "); // fetch data from database
                     $row = mysqli_fetch_array($img);
 
                     if (
-                        $row['picture'] == '' ||  $row['picture'] == null ||  empty($row['picture']) ||  !$row['picture'])
-                        {
-                        ?>
+                        $row['picture'] == '' || $row['picture'] == null || empty($row['picture']) || ! $row['picture']) {
+                    ?>
                         <img src="../images/user.png" alt="User Photo" width="45%"> <!-- This Dummy image will be displayed if user img not found in DB -->
                         <?php
-                    }
-                    else {
-                        echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['picture'] ).'" width="100" eight="100"/>';
-                    }
-                    ?>
-                    <span style="display: block;">Welcome <?php echo $_SESSION['name'] ?></span>
+                            } else {
+                                // echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['picture'] ).'" width="100" eight="100"/>';
+                                echo '<img src= "' . $row["picture"] . '" width="100" eight="100"/>';
+                            }
+                        ?>
+                    <span style="display: block;">Welcome<?php echo $_SESSION['name'] ?></span>
                 </div>
                 <hr style="border-width:1px;width:95%;text-align:center">
                 <a href="../admin/dashboard.php">Dashboard</a>
                 <a href="../admin/employees.php">Employees</a>
                 <a href="../admin/departments.php">Departments</a>
                 <a href="../admin/payrolls.php">Payrolls</a>
-                <a href="../admin/users.php">Users</a>            
+                <a href="../admin/users.php">Users</a>
                 <a href="../logout.php">Logout</a>
             </div>
         </div>
         <div class="task_area">
             <div class="bg_task_area">
-                <input class="searchbar_prl_adm" type="text" placeholder="Search Payroll...">
-                <hr style="border-width:1px;width:90%;text-align:center">
-                <table>
+
+                <input class="searchbar_emp_adm" type="text" placeholder="Search Payroll...">
+                <hr style="border-width:1px;width:100%;text-align:center ; margin:2%;">
+                <table style="width: 85%; font-size: 16px;margin-left:7%">
                     <tr>
                         <th>Sr. No</th>
                         <th>Emp. ID</th>
@@ -73,7 +80,7 @@
                         <th>Position</th>
                         <th>Presents</th>
                         <th>Absents</th>
-                        <th>Basic Salary</th>                        
+                        <th>Basic Salary</th>
                         <th>Earned Salary</th>
                         <th>Allowance</th>
                         <th>Deduction (Leaves)</th>
@@ -82,51 +89,51 @@
                     </tr>
                     <?php
 
-                    /**** PHP for attendance counts, absent counts, deduction for leaves count etc   *********/
-                    $today = date('Y-m-d');
-                    $first = date('Y-m-00');
-                    $attend = mysqli_query($con,"SELECT count(*) AS count FROM attendance WHERE attend_date > '$first'");
-                    $row = mysqli_fetch_array($attend);
-                    $attendCount = $row['count'];
+                        /**** PHP for attendance counts, absent counts, deduction for leaves count etc   *********/
+                        $today       = date('Y-m-d');
+                        $first       = date('Y-m-00');
+                        $attend      = mysqli_query($con, "SELECT count(*) AS count FROM attendance WHERE attend_date > '$first'");
+                        $row         = mysqli_fetch_array($attend);
+                        $attendCount = $row['count'];
 
-                    //   PHP to count absents in working days
-                    $myTime = strtotime(date('Y-m-d'));
-                    $workDays = 0;
-                    $days = date('d');
-                    while($days > 0)
-                    {
-                        $day = date("D", $myTime); // Sun - Sat
-                        if($day != "Sun")
-                            $workDays++;
+                        //   PHP to count absents in working days
+                        $myTime   = strtotime(date('Y-m-d'));
+                        $workDays = 0;
+                        $days     = date('d');
+                        while ($days > 0) {
+                            $day = date("D", $myTime); // Sun - Sat
+                            if ($day != "Sun") {
+                                $workDays++;
+                            }
 
-                        $days--;
-                        $myTime += 86400; // 86,400 seconds = 24 hrs.
-                    }
-                    $daysInMonth = cal_days_in_month(CAL_GREGORIAN, date('m'), date('Y'));
-                    $workDaysMonth = 0;
-                    // PHP to count total working days in a month
-                    while($daysInMonth > 0)
-                    {
-                        $day = date("D", $myTime); // Sun - Sat
-                        if($day != "Sun")
-                            $workDaysMonth++;
+                            $days--;
+                            $myTime += 86400; // 86,400 seconds = 24 hrs.
+                        }
+                        $daysInMonth   = cal_days_in_month(CAL_GREGORIAN, date('m'), date('Y'));
+                        $workDaysMonth = 0;
+                        // PHP to count total working days in a month
+                        while ($daysInMonth > 0) {
+                            $day = date("D", $myTime); // Sun - Sat
+                            if ($day != "Sun") {
+                                $workDaysMonth++;
+                            }
 
-                        $daysInMonth--;
-                        $myTime += 86400; // 86,400 seconds = 24 hrs.
-                    }
-                    $absentCount = $workDays - $attendCount;
+                            $daysInMonth--;
+                            $myTime += 86400; // 86,400 seconds = 24 hrs.
+                        }
+                        $absentCount = $workDays - $attendCount;
 
-                    $percent = 100 * $attendCount / $workDays;
+                        $percent = 100 * $attendCount / $workDays;
 
-                    $result = mysqli_query($con,"SELECT * FROM ((((employees
+                        $result = mysqli_query($con, "SELECT * FROM ((((employees
                             INNER JOIN positions ON positions.pos_id = employees.pos_id)
                             INNER JOIN allowances ON allowances.emp_id = employees.emp_id)
                             INNER JOIN deductions ON deductions.emp_id = employees.emp_id)
                             INNER JOIN salaries ON salaries.pos_id = positions.pos_id)
                             ORDER BY employees.emp_id");
-                    $i=1;
-                    while($row = mysqli_fetch_array($result)){
-                    ?>
+                        $i = 1;
+                        while ($row = mysqli_fetch_array($result)) {
+                        ?>
                     <tr>
                         <td>
                             <?php echo $i ?>
@@ -134,88 +141,89 @@
 
                         <td>
                             <?php
-                            $emp_id = $row['emp_id'];
-                            echo $emp_id?>
+                                $emp_id = $row['emp_id'];
+                                echo $emp_id?>
 
                         </td>
 
                         <td>
-                            <?php echo $row['name']?>
+                            <?php echo $row['name'] ?>
                         </td>
 
                         <td>
-                            <?php echo $row['pos_name']?>
-                        </td>
-                        
-                        <td>
-                            <?php
-                            $attend = mysqli_query($con,"SELECT count(*) AS count FROM attendance WHERE attend_date > '$first' AND emp_id = $emp_id");
-                            $rows = mysqli_fetch_array($attend);
-                            $attendCount = $rows['count'];
-                            echo $attendCount;
-                            ?>
+                            <?php echo $row['pos_name'] ?>
                         </td>
 
                         <td>
                             <?php
-                            $myTime = strtotime(date('Y-m-d'));
-                            $workDays = 0;
-                            $days = date('d');
-                            while($days > 0)
-                            {
-                                $day = date("D", $myTime); // Sun - Sat
-                                if($day != "Sun")
-                                    $workDays++;
-        
-                                $days--;
-                                $myTime += 86400; // 86,400 seconds = 24 hrs.
-                            }
-                            $absentCount = $workDays - $attendCount;
-                            echo $absentCount;
-                            ?>
-                        </td>
-
-                        <td>
-                            <?php echo $row['amount']?>
+                                $attend      = mysqli_query($con, "SELECT count(*) AS count FROM attendance WHERE attend_date > '$first' AND emp_id = $emp_id");
+                                    $rows        = mysqli_fetch_array($attend);
+                                    $attendCount = $rows['count'];
+                                    echo $attendCount;
+                                ?>
                         </td>
 
                         <td>
                             <?php
-                            $sal_per_day = $row['amount'] / $workDaysMonth;
-                            $earned = $attendCount * $sal_per_day;
-                            echo round($earned,2);
-                            ?>
+                                $myTime   = strtotime(date('Y-m-d'));
+                                    $workDays = 0;
+                                    $days     = date('d');
+                                    while ($days > 0) {
+                                        $day = date("D", $myTime); // Sun - Sat
+                                        if ($day != "Sun") {
+                                            $workDays++;
+                                        }
+
+                                        $days--;
+                                        $myTime += 86400; // 86,400 seconds = 24 hrs.
+                                    }
+                                    $absentCount = $workDays - $attendCount;
+                                    echo $absentCount;
+                                ?>
                         </td>
-                        
+
                         <td>
-                            <?php echo $row['allowance']?>
+                            <?php echo $row['amount'] ?>
+                        </td>
+
+                        <td>
+                            <?php
+                                $sal_per_day = $row['amount'] / $workDaysMonth;
+                                    $earned      = $attendCount * $sal_per_day;
+                                    echo round($earned, 2);
+                                ?>
+                        </td>
+
+                        <td>
+                            <?php echo $row['allowance'] ?>
                         </td>
 
                         <td>
                             <?php
                                 $ded = $sal_per_day * $absentCount;
-                                echo round($ded,2);
-                            ?>
+                                    echo round($ded, 2);
+                                ?>
                         </td>
 
                         <td>
                             <?php
                                 $net_sal = $earned + $row['allowance'];
-                                echo round($net_sal,2);
-                            ?>
+                                    echo round($net_sal, 2);
+                                ?>
                         </td>
 
                         <td>
-                            
+
                         </td>
                     </tr>
-                    <?php 
-                    $i++ ;
-                    } 
+                    <?php
+                        $i++;
+                        }
                     ?>
                 </table>
             </div>
         </div>
-    </div>    
+    </div>
+    <script src="../js/script.js"> </script>
 </body>
 </html>
